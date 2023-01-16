22f4b0eeeb8b4ecf8a689ad178753372f185a87f
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2023-01-16 19:43:38 +0900

    80028

--- AJIT-new/경시대회-2/Low/80028.cpp
+++ AJIT-new/경시대회-2/Low/80028.cpp
@@ -4,12 +4,54 @@ https://43.200.211.173/contest/18/problem/80028
 */
 #include <iostream>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
-int n, m, i;
-int* dmg;
+int n, m, i, j;
+int* arr;
+int dpmin(int* arr, int off)
+{
+	int min = 999999;
+	for (int i = 0; i < 3; i++) if (arr[i] < min && arr[i] >= 0) min = arr[i];
+	return min == 999999 ? -1 : (min + off);
+}
 int main()
 {
 	cin >> n >> m;
-	for (i = 0; i < n; i++) cin >> dmg[i];
+	int** dp = new int* [n + 2]{ 0 };
+	arr = new int[n+1] {0}; // dp[n+1]칸에 총 결과가 저장되는데, 이때 arr[n]에 엑세스하게 되면서 오류가 나는 것을 방지하기 위해 arr를 n+1의 크기로 초기화
+	for (i = 0; i <= n + 1; i++) dp[i] = new int[3]{ 0 };
+	for (i = 0; i < n; i++) cin >> arr[i];
+	for (i = 1; i <= n+1; i++)
+	{
+		if (arr[i - 1] < 0) // 현재 칸에 돌다리 없음
+			fill(dp[i], dp[i] + 3, -1);
+		else
+		{
+			dp[i][0] = dpmin(dp[i - 1], arr[i - 1]); // 한 칸 점프
+			dp[i][1] = i - 2 >= 0 ? dpmin(dp[i - 2], arr[i - 1]) : dp[i][0]; // 두 칸 점프
+			dp[i][2] = i - 3 >= 0 ? dpmin(dp[i - 3], arr[i - 1]) : dp[i][1]; // 세 칸 점프
+		}
+	}
 
+	int md = dpmin(dp[n+1], 0);
+
+	//constexpr int pad = 3;
+	//cout << "arr=      ";
+	//for (i = 0; i < n; i++)
+	//	cout << setw(pad) << arr[i] << ' ';
+	//cout << endl;
+	//
+	//for (j = 0; j < 3; j++)
+	//{
+	//	cout << "dp[" << j << "]=";
+	//	for (i = 0; i <= n+1; i++)
+	//		cout << setw(pad) << dp[i][j] << ' ';
+	//	cout << endl;
+	//}
+	//cout << "min_dmg=" << md << endl;
+
+	if (m - md >= 0)
+		cout << md << endl;
+	else
+		cout << -1 << endl;
 }
--- AJIT-new/경시대회-2/Low/80029.cpp
+++ AJIT-new/경시대회-2/Low/80029.cpp
@@ -4,7 +4,66 @@ https://43.200.211.173/contest/18/problem/80029
 */
 #include <iostream>
 #include <algorithm>
+#include <climits>
+#include <vector>
 using namespace std;
+typedef unsigned long long ll;
+int n, m, i, j, * ans;
+typedef struct _s
+{
+	int i, j, k;
+} s;
+s* arr;
+int jcmp(s a, s b)
+{
+	return a.j > b.j;
+}
+int icmp(s a, s b)
+{
+	return a.i > b.i;
+}
+// in: 4 8
+//     2 3 1 8
+// out: 2 3 1 2   or   2 2 1 3
 int main()
 {
+	cin >> n >> m;
+	arr = new s[n];
+	for (i = 0; i < n; i++)
+	{
+		cin >> j; arr[i] = { i, j, 0 };
+	}
+	stable_sort(arr, arr + n, jcmp);
+	ll total = 0;
+	int height = 0;
+	for (i = 0; i < n; i++)
+	{
+		int hdelta = arr[i].j - height;
+		int usage = 0;
+		for (j = hdelta; j > 0; j--)
+		{
+			ll newtotal = total + (ll)(n - i) * j;
+			if (newtotal <= m)
+			{
+				total = newtotal;
+				usage = j;
+				break;
+			}
+		}
+		if (j == 0)
+		{
+			usage = 0;
+			for (j = 0; j < (n - i) && total < m; j++)
+			{
+				total++;
+				usage++;
+			}
+		}
+		height += arr[i].j;
+		arr[i].k = usage;
+	}
+	sort(arr, arr + n, icmp);
+	for (int i = 0; i < n; i++)
+		cout << arr[i].k << ' ';
+	cout << endl;
 }
