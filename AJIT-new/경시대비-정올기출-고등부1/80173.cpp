6f0152fa92c6b553b7ad0c5959aeb61ba33198b4
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2023-02-19 13:34:18 +0900

    '80173 - 고집수' 해결 완료

--- AJIT-new/경시대비-정올기출-고등부1/80173.cpp
+++ AJIT-new/경시대비-정올기출-고등부1/80173.cpp
@@ -6,14 +6,17 @@ https://43.200.211.173/contest/17/problem/80173
 #include<cmath>
 #include<vector>
 using namespace std;
-int i, j, k, l, m, n, o, p, p1, p2;
+int i, j, k, l, m, n, o, p, q, p1, p2;
 vector<int>v;
 int decAndMul(int n)
 {
 	m = o = 1;
-	for (k = 0, l = log10(n); k <= l; k++)
+	for (k = 0, l = (int)log10(n); k <= l; k++)
 	{
-		o *= n / m % 10; // decompose와 multiplication을 동시에!
+		q = n / m % 10;
+		if (q == 0) // 더 이상 계산할 필요 x (모든 a에 대하여, a*0=0)
+			break;
+		o *= q; // decompose와 multiplication을 동시에!
 		m *= 10;
 	}
 	return o;
@@ -27,10 +30,9 @@ int main()
 		for (j = 1; j <= n; j++)
 		{
 			p = decAndMul(p);
-			if (p > 0 && p < 10 && j < n)
-				goto fail;
+			if (p < 10 && j != n) goto fail; // n번 반복하기 전에 한 자리 수가 되어버림 -> 실패
 		}
-		if (p > 0 && p < 10)//한자리 자연수
+		if (p < 10) // n번 반복한 후에도 한 자리 자연수가 아님
 			v.push_back(i);
 	fail:
 		;
