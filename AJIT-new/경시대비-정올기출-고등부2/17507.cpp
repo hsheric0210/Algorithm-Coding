652d243bf2fa2fc40a3c768470493d66f8a8dc15
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2023-03-01 19:00:49 +0900

    .

--- AJIT-new/경시대비-정올기출-고등부2/17507.cpp
+++ AJIT-new/경시대비-정올기출-고등부2/17507.cpp
@@ -5,9 +5,10 @@ https://43.200.211.173/contest/17/problem/17507
 #include<iostream>
 #include<cmath>
 #include<algorithm>
+#include<vector>
 using namespace std;
 int i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
-int a[32], b[32];
+int a[32], b[32], c[32];
 void build()
 {
 	b[1] = 1;
@@ -16,16 +17,36 @@ void build()
 		a[i] = (int)pow(2, i) - 1;
 		if (i > 1)
 			b[i] = b[i - 1] + (i - 1);
+		cout << "a[" << i << "]=" << a[i] << ", b[" << i << "]=" << b[i] << endl;
 	}
 }
-int func(int n, int depth)
+int brute_force(int n, int depth) // we can try brute-forcing when range is enoughly small
 {
 	if (n == 0) return depth;
-	auto lb = lower_bound(a, a + 30, n);
+	auto lb = lower_bound(a, a + 31, n);
 	if (*lb != n)
 		lb--;
-	return func(n - (*lb), depth + (lb - a));
+	return brute_force(n - (*lb), depth + (lb - a));
 }
+int div_and_conq(int start, int depth)
+{
+	c[0] = -1;
+	for (j = 1; j < 31; j++) // init table
+	{
+		c[j] = start + a[j];
+		//cout << start << " + " << "(a[" << j << "]=" << a[j] << ") = " << c[j] << endl;
+	}
+	auto lb = lower_bound(c, c + 31, y);
+	if (*lb == y)
+	{
+		//cout << "found exact on i=" << lb - c << ", val=" << *lb << endl;
+		return depth + (lb - c); // found exact
+	}
+	lb--;
+	//cout << "found on i=" << lb - c << ", val=" << *lb << ", original_depth=" << depth << ", depth_addition=" << lb - c << endl;
+	return div_and_conq(*lb, depth + (lb - c)); // found green
+}
+vector<int>deltas;
 int main()
 {
 	build();
@@ -35,33 +56,45 @@ int main()
 		cin >> x >> y;
 		if (x == y)
 		{
-			cout << func(y, 0) << endl;
+			cout << brute_force(y, 0) << endl;
 		}
 		else
 		{
 			auto lb = lower_bound(a, a + 31, y);
-			if (*lb == y)
+			m = max(x - 1, a[lb - a - 1]);
+			l = div_and_conq(m, lb - a - 1);
+			s = *max_element(b, b + (lb - a));
+			cout << "divcnq=" << l << ", b[i-1]=" << s << endl;
+			int sol1 = max(s, l);
+			cout << sol1 << endl; // b(i-1) 이전의 수들은 어차피 싹다 b(i-1)보다 작을 것이므로 볼 필요 없다.
+			q = 0;
+			for (p = x; p <= y; p++)
 			{
-				cout << lb - a << endl;
+				r = brute_force(p, 0);
+				//cout << "j(" << p << ")=" << r << endl;
+				if (r > q)
+					q = r;
 			}
-			else if (*lb == y + 1)
+			w = 0;
+			for (u = a[lb - a - 1]+1; u <= y; u++)
 			{
-				cout << b[lb - a] << endl;
+				v = brute_force(p, 0);
+				cout << "#2 j(" << u << ")=" << v << endl;
+				if (v > w)
+					w = v;
 			}
-			else
+			if (sol1 != q)
 			{
-				l = 0;
-				cout << "a[i] = " << a[lb - a] << ", a[i-1]=" << a[lb - a - 1] << endl;
-				cout << "b[i] = " << b[lb - a] << ", b[i-1]=" << b[lb - a - 1] << endl;
-				cout << "from " << a[lb - a - 1] + 1 << " to " << y << " -> distance " << y - (a[lb - a - 1] + 1) << endl;
-				for (p = a[lb - a - 1] + 1; p <= y; p++)
-				{
-					q = func(p, 0);
-					if (q > l)
-						l = q;
-				}
-				cout << max(b[lb - a - 1], l) << endl;
+				cout << "[!] max value mismatched on: range=" << x << ".." << y << " -> thought_solution=" << sol1 << ", brute_forced=" << q << ", delta=" << sol1 - q << endl;
+				//deltas.push_back(sol1 - q);
+			}
+			if (l != w)
+			{
+				cout << "[!] div_n_cnq mismatched on: range=" << m+1 << ".." << y << " -> dnq=" << l << ", brute_forced=" << w << ", delta=" << l - w << endl;
+				deltas.push_back(l-w);
 			}
 		}
 	}
+	for (auto a = deltas.begin(); a != deltas.end(); a++)
+		cout << "Delta=" << *a << endl;
 }
--- AJIT-new/경시대비-정올기출-고등부2/80039.cpp
+++ AJIT-new/경시대비-정올기출-고등부2/80039.cpp
@@ -1,49 +1,66 @@
 /*
-햄버거 분배
-https://43.200.211.173/contest/17/problem/1345
+최소 횟수 에디터
+https://43.200.211.173/contest/17/problem/80039
 */
 #include<iostream>
 #include<algorithm>
+#include<stack>
+#include<tuple>
+#include<vector>
 using namespace std;
-int i, j, k, l, m, n, o, p, q, r, s, t, u, v;
-char str[20005];
-int chosen[20005];
+int i, j, k, l, m;
+char s1[1005], s2[1005];
+vector<char>modbuf;
+int ed[1005][1005];
 int main()
 {
-	cin >> n >> k;
-	cin >> str;
-
-	for (i = 0; i < n; i++)
+	cin.getline(s1, 1005);
+	cin.getline(s2, 1005);
+	
+	for (i = 1; i < 1005; i++)
+		ed[i][0] = ed[0][i] = i;
+	for (i = 0; s1[i]; i++)
 	{
-		if (str[i] == 'H') // HaMbUrGuR
+		modbuf.push_back(s1[i]);
+		for (j = 0; s2[j]; j++)
+			ed[i + 1][j + 1] = min({ ed[i + 1][j] + 1, ed[i][j + 1] + 1, ed[i][j] + (s1[i] != s2[j]) });
+	}
+	modbuf.push_back(0);
+	cout << ed[i][j] << endl;
+
+	// start backtracking
+	stack<pair<int, int>>s;
+	s.push(make_pair(i, j));
+	while (!s.empty())
+	{
+		auto top = s.top();
+		//s.pop();
+		int a = top.first, b = top.second;
+		if (a == 0 || b == 0)
+			break;
+		if (s1[a - 1] == s2[b - 1])
+			s.push(make_pair(a - 1, b - 1)); // nothing changes
+		else
 		{
-			for (j = min(i + k, 20000); j >= i + 1; j--)
+			initializer_list<tuple<int, int, int, int>> ia = { {ed[a][b - 1], a, b - 1, 1}, {ed[a - 1][b], a - 1, b, 2}, {ed[a - 1][b - 1], a - 1, b - 1, 3} };
+			auto xelem = max(ia, [](tuple<int, int, int, int> f, tuple<int, int, int, int> s) {return get<0>(f) < get<0>(s); });
+			s.push(make_pair(get<1>(xelem), get<2>(xelem)));
+			switch (get<3>(xelem))
 			{
-				if (str[j] == 'P')
-				{
-					chosen[j] = 1;
-					l++;
-					break;
-				}
+			case 1: // insert
+				cout << modbuf._Unchecked_begin() << endl;
+				modbuf.insert(modbuf.begin() + a, s1[a]);
+				break;
+			case 2: // remove
+				cout << modbuf._Unchecked_begin() << endl;
+				modbuf.erase(modbuf.begin() + a);
+				break;
+			case 3: // substit.
+				cout << modbuf._Unchecked_begin() << endl;
+				modbuf[a] = s1[a];
+				break;
 			}
 		}
 	}
-
-	for (i = 0; i < n; i++)
-	{
-		if (str[i] == 'H') // HaMbUrGuR
-		{
-			for (j = max(i - k, 0); j <= i - 1; j++)
-			{
-				if (str[j] == 'P')
-				{
-					chosen[j] = 1;
-					m++;
-					break;
-				}
-			}
-		}
-	}
-	cout << "l=" << l << ", m=" << m << endl;
-	cout << max(l, m) << endl;
+	cout << modbuf._Unchecked_begin() << endl;
 }
