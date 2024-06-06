b90236e5b028f3bd586d3f560ab0428cabfa232e
eric0210 <77634181+hsheric0210@users.noreply.github.com>
2024-06-06 14:10:08 +0900

    3차 시간제한 Template

---
+++ cpp/AJIT-new/ICT 경시대회-2-시간제한3차/10.9785_음악프로그램.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+#include<vector>
+#include<list>
+#include<numeric>
+#include<deque>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	return 0;
+}
